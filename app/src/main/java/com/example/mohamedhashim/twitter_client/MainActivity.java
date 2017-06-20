7ceb570ad4858dc62702591a833fcd0aa50c5278
MohamedHashim <sawmido2000@gmail.com>
2017-06-20 16:24:57 +0200

    Save account credentials

--- app/src/main/java/com/example/mohamedhashim/twitter_client/MainActivity.java
+++ app/src/main/java/com/example/mohamedhashim/twitter_client/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.mohamedhashim.twitter_client;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.InputStream;
+
+import twitter4j.IDs;
+import twitter4j.StatusUpdate;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
@@ -19,10 +33,22 @@ import twitter4j.conf.ConfigurationBuilder;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    public static final int WEBVIEW_REQUEST_CODE = 100;
+    private static final String PREF_NAME = "sample_twitter_pref";
+    private static final String PREF_KEY_OAUTH_TOKEN = "oauth_token";
+    private static final String PREF_KEY_OAUTH_SECRET = "oauth_token_secret";
+    private static final String PREF_KEY_TWITTER_LOGIN = "is_twitter_loggedin";
+    private static final String PREF_USER_NAME = "twitter_user_name";
 
+    public static final int WEBVIEW_REQUEST_CODE = 100;
     private static Twitter twitter;
     private static RequestToken requestToken;
+
+    private static SharedPreferences sharedPreferences;
+
+    private TextView userName;
+    private View loginLayout;
+    private View follwersLayout;
+
     private String consumerKey = null;
     private String consumerSecret = null;
     private String callbackUrl = null;
@@ -39,12 +65,56 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         setContentView(R.layout.activity_main);
 
+        loginLayout = (RelativeLayout) findViewById(R.id.login_layout);
+        follwersLayout = (LinearLayout) findViewById(R.id.followers_layout);
+        userName = (TextView) findViewById(R.id.user_name);
+
         findViewById(R.id.btn_login).setOnClickListener(this);
 
         if (TextUtils.isEmpty(consumerKey) || TextUtils.isEmpty(consumerSecret)) {
             Toast.makeText(this, "Twitter key or secret not configured",
                     Toast.LENGTH_SHORT).show();
             return;
+        }
+
+        sharedPreferences = getSharedPreferences(PREF_NAME, 0);
+
+        boolean isLoggedIn = sharedPreferences.getBoolean(PREF_KEY_TWITTER_LOGIN, false);
+
+        if (isLoggedIn) {
+            loginLayout.setVisibility(View.GONE);
+            follwersLayout.setVisibility(View.VISIBLE);
+
+            String username = sharedPreferences.getString(PREF_USER_NAME, "");
+            userName.setText(getResources().getString(R.string.hello) + " " + username);
+        } else {
+            loginLayout.setVisibility(View.VISIBLE);
+            follwersLayout.setVisibility(View.GONE);
+
+            Uri uri = getIntent().getData();
+
+            if (uri != null && uri.toString().startsWith(callbackUrl)) {
+
+                String verifier = uri.getQueryParameter(oAuthVerifier);
+
+                try {
+
+                    AccessToken accessToken = twitter.getOAuthAccessToken(requestToken, verifier);
+                    long userId = accessToken.getUserId();
+                    final User user = twitter.showUser(userId);
+                    Log.d("use", String.valueOf(userId));
+                    final String username = user.getName();
+
+                    saveTwitterInfo(accessToken);
+
+                    loginLayout.setVisibility(View.GONE);
+                    follwersLayout.setVisibility(View.VISIBLE);
+                    userName.setText(getString(R.string.hello) + " " + username);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
@@ -55,23 +125,54 @@ public class MainActivity extends Activity implements View.OnClickListener {
         oAuthVerifier = getString(R.string.twitter_oauth_verifier);
     }
 
-    private void loginToTwitter() {
-        final ConfigurationBuilder builder = new ConfigurationBuilder();
-        builder.setOAuthConsumerKey(consumerKey);
-        builder.setOAuthConsumerSecret(consumerSecret);
+    private void saveTwitterInfo(AccessToken accessToken) {
 
-        final Configuration configuration = builder.build();
-        final TwitterFactory factory = new TwitterFactory(configuration);
-        twitter = factory.getInstance();
+        long userId = accessToken.getUserId();
+
+        User user;
 
         try {
-            requestToken = twitter.getOAuthRequestToken(callbackUrl);
 
-            final Intent intent = new Intent(this, WebViewActivity.class);
-            intent.putExtra(WebViewActivity.EXTRA_URL, requestToken.getAuthenticationURL());
-            startActivityForResult(intent, WEBVIEW_REQUEST_CODE);
+            user = twitter.showUser(userId);
+            String username = user.getName();
+
+            SharedPreferences.Editor e = sharedPreferences.edit();
+            e.putString(PREF_KEY_OAUTH_TOKEN, accessToken.getToken());
+            e.putString(PREF_KEY_OAUTH_SECRET, accessToken.getTokenSecret());
+            e.putBoolean(PREF_KEY_TWITTER_LOGIN, true);
+            e.putString(PREF_USER_NAME, username);
+            e.commit();
+
         } catch (TwitterException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void loginToTwitter() {
+
+        boolean isLoggedIn = sharedPreferences.getBoolean(PREF_KEY_TWITTER_LOGIN, false);
+
+        if (!isLoggedIn) {
+            final ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.setOAuthConsumerKey(consumerKey);
+            builder.setOAuthConsumerSecret(consumerSecret);
+
+            final Configuration configuration = builder.build();
+            final TwitterFactory factory = new TwitterFactory(configuration);
+            twitter = factory.getInstance();
+
+            try {
+                requestToken = twitter.getOAuthRequestToken(callbackUrl);
+
+                final Intent intent = new Intent(this, WebViewActivity.class);
+                intent.putExtra(WebViewActivity.EXTRA_URL, requestToken.getAuthenticationURL());
+                startActivityForResult(intent, WEBVIEW_REQUEST_CODE);
+            } catch (TwitterException e) {
+                e.printStackTrace();
+            }
+        } else {
+            loginLayout.setVisibility(View.GONE);
+            follwersLayout.setVisibility(View.VISIBLE);
         }
     }
 
@@ -88,7 +189,14 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 final User user = twitter.showUser(userId);
                 String username = user.getName();
 
+                saveTwitterInfo(accessToken);
+
+                loginLayout.setVisibility(View.GONE);
+                follwersLayout.setVisibility(View.VISIBLE);
+
                 Toast.makeText(getApplicationContext(), "Welcome Mr." + username, Toast.LENGTH_LONG).show();
+                userName.setText(MainActivity.this.getResources().getString(R.string.hello)
+                        + " " + username);
 
             } catch (Exception e) {
                 e.printStackTrace();
