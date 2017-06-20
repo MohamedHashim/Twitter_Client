d30af1857e45393b94e55d6b251d8ecd81b4c96c
MohamedHashim <sawmido2000@gmail.com>
2017-06-20 14:00:18 +0200

    Add login function with twitter authentication

--- app/src/main/java/com/example/mohamedhashim/twitter_client/MainActivity.java
+++ app/src/main/java/com/example/mohamedhashim/twitter_client/MainActivity.java
@@ -1,13 +1,108 @@
 package com.example.mohamedhashim.twitter_client;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.StrictMode;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.User;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    public static final int WEBVIEW_REQUEST_CODE = 100;
+
+    private static Twitter twitter;
+    private static RequestToken requestToken;
+    private String consumerKey = null;
+    private String consumerSecret = null;
+    private String callbackUrl = null;
+    private String oAuthVerifier = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        initTwitterConfigs();
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_login).setOnClickListener(this);
+
+        if (TextUtils.isEmpty(consumerKey) || TextUtils.isEmpty(consumerSecret)) {
+            Toast.makeText(this, "Twitter key or secret not configured",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+    private void initTwitterConfigs() {
+        consumerKey = getString(R.string.twitter_consumer_key);
+        consumerSecret = getString(R.string.twitter_consumer_secret);
+        callbackUrl = getString(R.string.twitter_callback);
+        oAuthVerifier = getString(R.string.twitter_oauth_verifier);
+    }
+
+    private void loginToTwitter() {
+        final ConfigurationBuilder builder = new ConfigurationBuilder();
+        builder.setOAuthConsumerKey(consumerKey);
+        builder.setOAuthConsumerSecret(consumerSecret);
+
+        final Configuration configuration = builder.build();
+        final TwitterFactory factory = new TwitterFactory(configuration);
+        twitter = factory.getInstance();
+
+        try {
+            requestToken = twitter.getOAuthRequestToken(callbackUrl);
+
+            final Intent intent = new Intent(this, WebViewActivity.class);
+            intent.putExtra(WebViewActivity.EXTRA_URL, requestToken.getAuthenticationURL());
+            startActivityForResult(intent, WEBVIEW_REQUEST_CODE);
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode == Activity.RESULT_OK) {
+            String verifier = data.getExtras().getString(oAuthVerifier);
+
+            try {
+                AccessToken accessToken = twitter.getOAuthAccessToken(requestToken, verifier);
+
+                long userId = accessToken.getUserId();
+                final User user = twitter.showUser(userId);
+                String username = user.getName();
+
+                Toast.makeText(getApplicationContext(), "Welcome Mr." + username, Toast.LENGTH_LONG).show();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_login:
+                loginToTwitter();
+                break;
+        }
     }
 }
